a18e356d768365bdf89b3ec7c0e625188ec739a3
DarkCompet <darkcompet@gmail.com>
2024-06-30 18:02:17 +0700

    Add group by method

--- src/CollectionExt.cs
+++ src/CollectionExt.cs
@@ -10,8 +10,13 @@ namespace Tool.Compet.Core {
 			return list == null || list.Count == 0;
 		}
 
+		/// <summary>
 		/// This perform optimization by remove last item instead, so array will not be shrinked.
 		/// We recommend use this if you don't care item-order after removed.
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="list"></param>
+		/// <param name="index"></param>
 		public static void FastRemoveDk<T>(this List<T> list, int index) {
 			var lastIndex = list.Count - 1;
 			if (index >= 0) {
@@ -23,6 +28,28 @@ namespace Tool.Compet.Core {
 					list.RemoveAt(lastIndex);
 				}
 			}
+		}
+
+		/// <summary>
+		/// Convert collection `values` to map of: key, values.
+		/// </summary>
+		/// <typeparam name="TKey"></typeparam>
+		/// <typeparam name="TValue"></typeparam>
+		/// <param name="values"></param>
+		/// <param name="CalculateKey"></param>
+		/// <returns></returns>
+		public static Dictionary<TKey, List<TValue>> GroupByKeyDk<TKey, TValue>(this IEnumerable<TValue> values, Func<TValue, TKey> CalculateKey) where TKey : notnull {
+			var key2values = new Dictionary<TKey, List<TValue>>();
+			foreach (var item in values) {
+				var key = CalculateKey(item);
+				if (key2values.TryGetValue(key, out var list)) {
+					list.Add(item);
+				}
+				else {
+					key2values[key] = [item];
+				}
+			}
+			return key2values;
 		}
 	}
 }
