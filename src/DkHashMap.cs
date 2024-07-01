0408078c4255a293870011152de28f5a23da670c
DarkCompet <darkcompet@gmail.com>
2024-07-01 21:26:47 +0700

    Rename to dk hashmap

--- src/DkHashMap.cs
+++ src/DkHashMap.cs
@@ -1,7 +1,7 @@
 #pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
 namespace Tool.Compet.Core;
 
-public class HashMap<TKey, TValue> : Dictionary<TKey, TValue?> where TKey : notnull {
+public class DkHashMap<TKey, TValue> : Dictionary<TKey, TValue?> where TKey : notnull {
 	public new TValue? this[TKey key] {
 		get => this.GetValueOrDefault(key, default);
 		set => base[key] = value;
