0216f76c130d708b38aaffb35cdb93e6074bee11
DarkCompet <darkcompet@gmail.com>
2024-09-03 09:54:18 +0700

    Add heap ds

---
+++ src/DkHeap.cs
@@ -0,0 +1,5 @@
+#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
+namespace Tool.Compet.Core;
+
+public class DkHeap<TElement, TPriority> : PriorityQueue<TElement, TPriority> where TElement : notnull {
+}
