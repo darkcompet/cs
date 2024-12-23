f1fdb291829a1ab61fdaec142d07a5b8afbe21d7
DarkCompet <darkcompet@gmail.com>
2024-12-23 07:42:13 +0700

    Add ext for dictionary

---
+++ src/Extension/DictionaryExt.cs
@@ -0,0 +1,30 @@
+#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
+namespace Tool.Compet.Core;
+
+public static class DictionaryExt {
+	/// <summary>
+	/// Add (over-write for existed entry) given pairs to the dictionary.
+	/// </summary>
+	/// <typeparam name="TKey"></typeparam>
+	/// <typeparam name="TValue"></typeparam>
+	/// <param name="me"></param>
+	/// <param name="elements"></param>
+	public static void AddRangeDk<TKey, TValue>(this Dictionary<TKey, TValue> me, Dictionary<TKey, TValue> elements) {
+		foreach (var (key, value) in elements) {
+			me[key] = value;
+		}
+	}
+
+	/// <summary>
+	/// Try add (skip over-write for existed entry) given pairs to the dictionary.
+	/// </summary>
+	/// <typeparam name="TKey"></typeparam>
+	/// <typeparam name="TValue"></typeparam>
+	/// <param name="me"></param>
+	/// <param name="elements"></param>
+	public static void TryAddRangeDk<TKey, TValue>(this Dictionary<TKey, TValue> me, Dictionary<TKey, TValue> elements) {
+		foreach (var (key, value) in elements) {
+			me.TryAdd(key, value);
+		}
+	}
+}
