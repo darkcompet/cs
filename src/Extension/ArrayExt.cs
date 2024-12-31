6c8922245f8daca67a9ce95a626bf0262dee867d
DarkCompet <darkcompet@gmail.com>
2024-12-31 09:39:53 +0700

    Add join array function

--- src/Extension/ArrayExt.cs
+++ src/Extension/ArrayExt.cs
@@ -1,6 +1,8 @@
 #pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
 namespace Tool.Compet.Core;
 
+using System.Runtime.CompilerServices;
+
 /// <summary>
 /// Extension for array.
 /// </summary>
@@ -13,5 +15,10 @@ public static class ArrayExt {
 	/// <returns></returns>
 	public static bool IsEmptyDk<T>(this T[]? arr) {
 		return arr == null || arr.Length == 0;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static string JoinWithDk<T>(this T[] me, char separator) {
+		return string.Join(separator, me);
 	}
 }
