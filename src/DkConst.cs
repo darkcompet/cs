e249f40f7587828da9c25bc8a7f0157eeceb457c
DarkCompet <darkcompet@gmail.com>
2025-02-16 12:21:39 +0700

    Add dk const class

---
+++ src/DkConst.cs
@@ -0,0 +1,14 @@
+#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
+namespace Tool.Compet.Core;
+
+public class DkConst {
+	/// <summary>
+	/// Alphabet chars (a-zA-Z0-9).
+	/// </summary>
+	public const string Base62Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	/// <summary>
+	/// Letter chars (a-zA-Z).
+	/// </summary>
+	public const string Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+}
