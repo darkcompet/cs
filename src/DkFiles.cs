c9ef9acadfe68064aa560edc590a82a67e743eb6
DarkCompet <darkcompet@gmail.com>
2025-09-02 09:38:44 +0700

    Add file util

---
+++ src/DkFiles.cs
@@ -0,0 +1,29 @@
+#pragma warning disable IDE0130 // Namespace がフォルダー構造と一致しません
+namespace Tool.Compet.Core;
+
+using System.IO;
+using System.Threading.Tasks;
+
+public static class DkFiles {
+	/// <summary>
+	/// It is useful for copy very large file with non-blocking IO approach (so don't consume/block thread pool thread).
+	/// </summary>
+	/// <param name="srcFilePath"></param>
+	/// <param name="dstFilePath"></param>
+	/// <param name="bufferSize"></param>
+	/// <returns></returns>
+	public static async Task CopyFileAsync(string srcFilePath, string dstFilePath, int bufferSize = 81920) {
+		// Ensure the destination directory exists
+		Directory.CreateDirectory(Path.GetDirectoryName(dstFilePath)!);
+
+		await using var srcStream = new FileStream(
+			srcFilePath, FileMode.Open, FileAccess.Read, FileShare.Read,
+			bufferSize: bufferSize, useAsync: true);
+
+		await using var dstStream = new FileStream(
+			dstFilePath, FileMode.Create, FileAccess.Write, FileShare.None,
+			bufferSize: bufferSize, useAsync: true);
+
+		await srcStream.CopyToAsync(dstStream, bufferSize);
+	}
+}
